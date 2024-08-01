98c3c535b33b55d53dbe5518b70e7a30a7eb18ac
Sergey_Valerievich <sergey.w.yakovenko@gmail.com>
2024-08-01 23:19:15 +0500

    task10

--- sem/task10.py
+++ sem/task10.py
@@ -10,7 +10,24 @@
 # data = pd.DataFrame({'whoAmI':lst})
 # data.head()
 
+import pandas as pd
 import random
 
-data = pd.DataFrame(random.sample(['robot', 'human'] * 10, 10), columns = {'WhoAmI'})
-data.head()
+# Генерация данных
+lst = ['red_panda'] * 10
+lst += ['white_panda'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+# Создание пустого DataFrame для one-hot кодирования
+one_hot = pd.DataFrame()
+
+# Получение уникальных значений
+unique_values = data['whoAmI'].unique()
+
+# Заполнение DataFrame значениями 0 и 1
+for value in unique_values:
+    one_hot[value] = (data['whoAmI'] == value).astype(int)
+
+# Вывод первых строк
+print(one_hot.head())
